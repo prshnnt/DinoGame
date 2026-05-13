20ca25fa12187bcf1f823281582c6b3d3292b884
Prashant Singh <prshnnt@gmail.com>
2026-05-13 23:41:46 +0530

    level: Support enemy types in level loader
    - Update level loader to parse enemy type from JSON
    - Support old format (just [x, y]) and new format (dict with type)

--- core/level.py
+++ core/level.py
@@ -2,10 +2,10 @@
 
 import json
 from entities.platform import Platform
-from entities.enemy import Enemy
+from entities.enemy import create_enemy
 
 class Level:
-    def __init__(self, filename,image):
+    def __init__(self, filename, image):
         with open(filename, "r") as f:
             data = json.load(f)
 
@@ -13,9 +13,17 @@ class Level:
         self.end_x = data["end_x"]
 
         self.platforms = [
-            Platform(*p,image) for p in data["platforms"]
+            Platform(*p, image) for p in data["platforms"]
         ]
 
-        self.enemies = [
-            Enemy(tuple(e)) for e in data["enemies"]
-        ]
+        # Load enemies with type specification
+        self.enemies = []
+        for enemy_data in data.get("enemies", []):
+            if isinstance(enemy_data, list):
+                # Old format: [x, y]
+                self.enemies.append(create_enemy("ground", tuple(enemy_data)))
+            elif isinstance(enemy_data, dict):
+                # New format: {"x": 100, "y": 200, "type": "ground"}
+                enemy_type = enemy_data.get("type", "ground")
+                pos = (enemy_data["x"], enemy_data["y"])
+                self.enemies.append(create_enemy(enemy_type, pos))
